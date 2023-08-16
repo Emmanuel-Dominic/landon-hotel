6d1082b31243c559c5e8c6d11dc8c778a91f39c4
Matembu Emmanuel Dominic <ematembu2@gmail.com>
2023-08-16 10:25:09 +0300

    configure datasource with postgresql

--- src/main/java/com/emmanuel/lil/landon_hotel/CLRunner.java
+++ src/main/java/com/emmanuel/lil/landon_hotel/CLRunner.java
@@ -1,26 +1,38 @@
 package com.emmanuel.lil.landon_hotel;
+import com.emmanuel.lil.landon_hotel.data.entity.Guest;
+import com.emmanuel.lil.landon_hotel.data.entity.Reservation;
 import com.emmanuel.lil.landon_hotel.data.entity.Room;
+import com.emmanuel.lil.landon_hotel.data.repository.GuestRepository;
+import com.emmanuel.lil.landon_hotel.data.repository.ReservationRepository;
 import com.emmanuel.lil.landon_hotel.data.repository.RoomRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
-import java.util.Optional;
 
 
 @Component
 public class CLRunner implements CommandLineRunner {
     private final RoomRepository roomRepository;
+    private final GuestRepository guestRepository;
+    private final ReservationRepository reservationRepository;
 
-    public CLRunner(RoomRepository roomRepository) {
+    public CLRunner(RoomRepository roomRepository, GuestRepository guestRepository, ReservationRepository reservationRepository) {
         this.roomRepository = roomRepository;
+        this.guestRepository = guestRepository;
+        this.reservationRepository = reservationRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
-        List<Room> rooms = roomRepository.findAll();
-        Optional<Room> room = this.roomRepository.findByRoomNumberIgnoreCase("p1"); // rooms.get(0).getId());
-        System.out.println(room);
+        System.out.println("*** GUESTS ***");
+        List<Guest> guests = this.guestRepository.findAll();
+        guests.forEach(System.out::println);
+        System.out.println("*** ROOMS ***");
+        List<Room> rooms = this.roomRepository.findAll();
         rooms.forEach(System.out::println);
+        System.out.println("*** RESERVATIONS ***");
+        List<Reservation> reservations = this.reservationRepository.findAll();
+        reservations.forEach(System.out::println);
     }
 }
---
+++ src/main/java/com/emmanuel/lil/landon_hotel/data/repository/ReservationRepository.java
@@ -0,0 +1,9 @@
+package com.emmanuel.lil.landon_hotel.data.repository;
+import com.emmanuel.lil.landon_hotel.data.entity.Reservation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Date;
+import java.util.List;
+
+public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+    List<Reservation> findAllByReservationDate(Date date);
+}
