d6de8449c33892a06029bd61965b952d7a5d409e
Matembu Emmanuel Dominic <ematembu2@gmail.com>
2023-08-18 08:21:02 +0300

    implement service tier

--- src/main/java/com/emmanuel/lil/landon_hotel/CLRunner.java
+++ src/main/java/com/emmanuel/lil/landon_hotel/CLRunner.java
@@ -1,10 +1,9 @@
 package com.emmanuel.lil.landon_hotel;
-import com.emmanuel.lil.landon_hotel.data.entity.Guest;
-import com.emmanuel.lil.landon_hotel.data.entity.Reservation;
-import com.emmanuel.lil.landon_hotel.data.entity.Room;
 import com.emmanuel.lil.landon_hotel.data.repository.GuestRepository;
 import com.emmanuel.lil.landon_hotel.data.repository.ReservationRepository;
 import com.emmanuel.lil.landon_hotel.data.repository.RoomRepository;
+import com.emmanuel.lil.landon_hotel.service.RoomReservationService;
+import com.emmanuel.lil.landon_hotel.service.model.RoomReservation;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -13,26 +12,14 @@ import java.util.List;
 
 @Component
 public class CLRunner implements CommandLineRunner {
-    private final RoomRepository roomRepository;
-    private final GuestRepository guestRepository;
-    private final ReservationRepository reservationRepository;
+    private final RoomReservationService roomReservationService;
 
-    public CLRunner(RoomRepository roomRepository, GuestRepository guestRepository, ReservationRepository reservationRepository) {
-        this.roomRepository = roomRepository;
-        this.guestRepository = guestRepository;
-        this.reservationRepository = reservationRepository;
+    public CLRunner(RoomRepository roomRepository, GuestRepository guestRepository, ReservationRepository reservationRepository, RoomReservationService roomReservationService) {
+        this.roomReservationService = roomReservationService;
     }
 
     @Override
     public void run(String... args) throws Exception {
-        System.out.println("*** GUESTS ***");
-        List<Guest> guests = this.guestRepository.findAll();
-        guests.forEach(System.out::println);
-        System.out.println("*** ROOMS ***");
-        List<Room> rooms = this.roomRepository.findAll();
-        rooms.forEach(System.out::println);
-        System.out.println("*** RESERVATIONS ***");
-        List<Reservation> reservations = this.reservationRepository.findAll();
-        reservations.forEach(System.out::println);
+        List<RoomReservation> reservations = this.roomReservationService.getRoomReservationForDate("2022-08-18");
     }
 }
